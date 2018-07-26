51079b8d7a3e519e77299370afb09ec8d000c0c0
Steven G. Johnson <stevenj@alum.mit.edu>
2018-07-26 12:32:32 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# NLoptBuilder
+
+[![Build Status](https://travis-ci.org/stevengj/NLoptBuilder.svg?branch=master)](https://travis-ci.org/stevengj/NLoptBuilder)
+
+This repository builds compiled binary libraries of [c-blosc](https://github.com/stevengj/nlopt) for the [NLopt.jl](https://github.com/stevengj/NLopt.jl) project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/stevengj/NLoptBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository uses [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl).
---
+++ build_tarballs.jl
@@ -0,0 +1,34 @@
+using BinaryBuilder
+
+# Collection of sources required to build NLopt
+sources = [
+    "https://github.com/stevengj/nlopt.git" =>
+    "c0f0ddb2cf6b494fbe7a92226ba15789f7788733", # v2.5 alpha
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/nlopt
+
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DNLOPT_CXX=On -DNLOPT_PYTHON=Off -DNLOPT_OCTAVE=Off -DNLOPT_GUILE=Off -DNLOPT_SWIG=Off ..
+make && make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms() # build on all supported platforms
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libnlopt", :libnlopt),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, "NLopt", sources, script, platforms, products, dependencies)
+
